7f0b4773f47b7c35979396058848f2b67e8176b2
Craig Roberts <craig.roberts@tesco.com>
2021-12-12 07:57:32 +0000

    day11 solved

--- Day11/part1.rb
+++ Day11/part1.rb
@@ -2,8 +2,8 @@ start = Time.now()
 
 DIRECTIONS = [ [-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1] ]
 
-def pause
-  puts "pause..."
+def pause(m = "pause")
+  puts "#{m}..."
   exit if STDIN.gets.chomp == 'x'
 end
 
@@ -11,13 +11,13 @@ def getInput(testing)
   if testing
     File.readlines("test.txt").map { |line| line.chomp.chars.map(&:to_i) }
   else
-    File.read("input.txt")
+    File.readlines("input.txt").map { |line| line.chomp.chars.map(&:to_i) }
   end
 end
 
 def addOne(octos)
-  (0...H).each do |y|
-    (0...W).each do |x|
+  (0...(octos.length)).each do |y|
+    (0...(octos[0].length)).each do |x|
       # puts "octo: #{octos[y][x]} => #{octos[y][x] += 1}"
       octos[y][x] += 1
     end
@@ -25,59 +25,67 @@ def addOne(octos)
   return octos
 end
 
-def getNeighbours(x, y, grid)
+def findNeighbours(x, y, grid)
   neighbours = []
   DIRECTIONS.each do |dir|
-    # puts "dir: #{dir}"
-    # puts "x: #{y}, y: #{y} => dir: #{dir}: #{grid[y + dir[1]][x + dir[0]]}"
-    if (y + dir[1] >= 0) && (x + dir[0] >= 0) && (y + dir[1] < H) && (x + dir[0] < W)
-      # puts "#{x}, #{y} neighbour is: #{y + dir[1]},#{x + dir[0]}, val: #{grid[y + dir[1]][x + dir[0]]}"
+    if (y + dir[1] >= 0) && (x + dir[0] >= 0) && (y + dir[1] < grid.length) && (x + dir[0] < grid[0].length)
       neighbours << [x + dir[0], y + dir[1]]
     end
   end
   return neighbours
 end
 
-def flash(x, y, octos, flashed)
-  puts "flash at #{x}, #{y}"
-  neighbours = getNeighbours(x, y, octos)
-  neighbours.each do |neighbour|
-    if !flashed.include?([neighbour[0]],[neighbour[1]])
-      octos[neighbour[0]][neighbour[1]] += 1
-    end
+def isFlashes(octos)
+  octos.flatten.sort.inject(0) { |k, v| v > 9 }
+end
+
+def flash(x, y, octos)
+  findNeighbours(x, y, octos).each do |neighbour|
+    octos[neighbour[0]][neighbour[1]] += 1
   end
 end
 
-octos = getInput(ARGV[0] == 't')
-H = octos.length
-W = octos[0].length
-part1 = 0
+def solve(part)
+  octos = getInput(ARGV[0] == 't')
+  h = octos.length
+  w = octos[0].length
+  solution = 0
+  limit = if part == 1
+            100
+          else
+            300
+          end
 
-pp octos, '----'
+  for step in 0...limit do
+    flashed = []
 
-for step in 0..10 do
-  flashed = []
+    octos = addOne(octos)
 
-  octos = addOne(octos)
-  pp octos, '----'
+    while isFlashes(octos) do
+      (0...h).each do |x|
+        (0...w).each do |y|
+          if octos[x][y] > 9 && !flashed.include?([x, y])
+            flash(x, y, octos)
+            flashed << [x, y]
+          end
+        end
+      end
 
-  (0...H).each do |x|
-    (0...W).each do |y|
-      if octos[x][y] > 9
-        flash(x, y, octos, flashed)
-        flashed << [x, y]
+      flashed.each do |xy|
+        octos[xy[0]][xy[1]] = 0
       end
     end
+
+    if part == 1
+      solution += octos.flatten.count(0)
+    elsif octos.flatten.count(0) == 100
+      pp "Step: #{step}"
+      pp octos
+      return step + 1
+    end
   end
-
-  flashed.each do |octo|
-    octos[octo[0]][octo[1]] = 0
-  end
-
-  puts "After step #{step+1}:\n"
-  pp octos
-
-  pause
+  return solution
 end
 
-puts "Part 1: #{part1} in #{Time.now - start} secs."
+puts "Part 1: #{solve(1)} in #{Time.now - start} secs."
+puts "Part 2: #{solve(2)} in #{Time.now - start} secs."
