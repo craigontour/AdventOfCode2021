8dec7f61d706e75d08be999f7cec839a539eaca4
Craig Roberts <craig.roberts@tesco.com>
2021-12-21 20:35:16 +0000

    day 20 parts.rb - uses int not string

--- Day20/parts.rb
+++ Day20/parts.rb
@@ -7,16 +7,15 @@ end
 
 def getData(f)
   data = File.read("#{f}.txt").split("\n\n")
-  alg = data[0]
+  alg = []
+  data[0].chars.each_with_index do |ch, i|
+    alg[i] = ch == '#' ? 1 : 0
+  end
 
   input = {}
   data[1].split("\n").each_with_index do |row, r|
     row.chars.each_with_index do |ch, c|
-      if row[c] == '#'
-        input[[c, r]] = 1
-      else
-        input[[c, r]] = 0
-      end
+      input[[c, r]] = 1 if row[c] == '#'
     end
   end
 
@@ -55,22 +54,21 @@ def getNine(input, x, y)
 end
 
 def main(alg, input, t)
-  
   t.times do |i|
     newinput = {}
+
     min = input.keys.min_by { |x, y| [x, y] }
     max = input.keys.max_by { |x, y| [x, y] }
+    # puts "min: #{min[0]}..#{min[1]}"
+    # puts "max: #{max[0]}..#{max[1]}"
 
-    ((min[1]-1)..(max[1])+1).each do |y|
-      ((min[0]-1)..(max[0])+1).each do |x|
-        if alg[getNine(input, x, y)] == '#'
-          newinput[[x, y]] = 1
-        else
-          newinput[[x, y]] = 0
-        end
+    ((min[1]-1)..(max[1]+1)).each do |y|
+      ((min[0]-1)..(max[0]+1)).each do |x|
+        newinput[[x, y]] = alg[getNine(input, x, y)]
       end
     end
-    input = newinput
+
+    input = newinput.clone
   end
   
   return input
@@ -83,3 +81,4 @@ pp part1.values.count(1)
 
 part2 = main(alg, input, 50)
 pp part2.values.count(1)
+# printGrid(part2)
