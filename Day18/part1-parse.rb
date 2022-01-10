a4d210d128e730d583e4c645ed824223a06ddcc3
Craig Roberts <craig.roberts@tesco.com>
2022-01-10 06:53:18 +0000

    day 18 2021 parse works

--- Day18/part1-parse.rb
+++ Day18/part1-parse.rb
@@ -1,11 +1,11 @@
 start = Time.now()
 
+debug = ARGV[1] == 'd'
+
 def pause(m = "pause")
   puts "#{m}..."
   exit if STDIN.gets.chomp == 'x'
 end
-
-lines = File.readlines("#{ARGV[0]}.txt").map(&:chomp)
 
 def updateLeft(num, i, oldline, debug)
   lhs = oldline.dup
@@ -51,95 +51,73 @@ def updateRight(num, oldi, oldline, debug)
 end
 
 def explode(i, ns, nums, line, debug)
-  puts "EXPLODE:
-  - ns  : #{ns}
-  - i   : #{i}
-  - nums: #{nums}
-  - line: #{line}
-
-  - line[0..ns-1]: #{line[0..ns-1] }
-  - line[ns..i]  : #{line[ns..i] }
-  - line[i+1..-1]: #{line[i+1..-1]}
-  " if debug
-  
   # line becomes left side of explode + 0 + right side of explode
   ul = updateLeft(nums[0], ns, line, debug)
   ur = updateRight(nums[1], i, line, debug)
 
-  puts "-- explode line: #{line} to #{ul + '0' + ur}" if debug
+  puts "-- explode: #{nums}
+  line: #{line}
+  to  : #{ul + '0' + ur}
+  "
+  pause
   return ul + '0' + ur
 end
 
 def split_number(ns, i, n, line, debug)
   a, b = n.to_i.divmod(2)
-  puts "
-  split_number:
-  ns  : #{ns}
-  i   : #{i}
-  n   : #{n}
-  a, b: #{a}, #{b}
 
-  lhs : #{line[0..ns]}
-  rhs : #{line[i+1..-1]}
-  line: #{line[0..ns]}[#{a},#{a+b}]#{line[i+1..-1]}
-  " if debug
+  puts "-- split number
+  line: #{line}
+  to  : #{line[0..ns] + "[#{a},#{a+b}]" + line[i+1..-1]}
+  "
+  pause
   return line[0..ns] + "[#{a},#{a+b}]" + line[i+1..-1]
 end
 
-debug = ARGV[1] == 'd'
+input = File.readlines("#{ARGV[0]}.txt").map(&:chomp)
+line = input[0]
 
-lines.each do |line|
-  # pp line.scan(/\[\d+,\d+\]/)
-  # pp line.match(/\[.*\[.*\[.*\[.*\[\d+,\d+\]/)
+(1..input.length-1).each do |l|
+  puts "--------------------"
+  line = '[' + line + ',' + input[l] + ']'
 
-  puts "---------- New line: #{line} ----------"
-  
   i = 0
   nums = []
   n = ''
-  lb = 0
-  rb = 0
+  br = 0
   num_start = 0
   start_line = ''
 
-  # Need to loop until all reductions done
-  # while start_line != line 
-  # set start_line to line, if not changed after this loop then done
   start_line = line.dup
 
   while i < line.length-1 do
-    puts "i:#{i}, line:#{line}"
+    puts "i:#{i}, line:#{line}" if debug
     ch = line[i]
 
     if ch == '['
-      lb += 1
-      explode = true if lb - rb == 5
-      puts "- open  : #{ch}, explode: #{explode ? 'Yes' : 'No'}" if debug
+      br += 1
+      explode = true if br == 5
       nums = [] if nums.length > 0
     elsif ch == ']'
-      puts "- close : #{ch}" if debug
-      rb += 1
+      br -= 1
       if n != '' && nums.length == 1 && explode
         nums << n.to_i
         line = explode(i, num_start, nums, line, debug)
         # reset vars
         i = -1
         nums = []
-        lb = 0
-        rb = 0
+        br = 0
         num_start = 0
         explode = false
       end
       n = ''
     elsif ch == ','
-      puts "- comma : #{ch}" if debug
       if n != ''
         nums << n.to_i
         n = ''
       end
     else
       num_start = i-1 if nums.length == 0 && explode
-      puts "- number: #{ch}, n: #{n}, num_start:#{num_start}, exploding?: #{explode}" if debug
       if n == ''
         n = ch
       else
@@ -150,8 +128,7 @@ lines.each do |line|
         # reset vars
         i = -1
         nums = []
-        lb = 0
-        rb = 0
+        br = 0
         num_start = 0
         n = ''
       end
